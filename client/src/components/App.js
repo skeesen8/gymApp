6e25ee24385a14b6416793b11036af66ed5763bf
Tom Golebiewski <tgolebie16@gmail.com>
2023-11-22 01:29:16 -0500

    changed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GymCard from './GymCard'
 // import Navbar from './Navbar'
 import AddGymForm from "./AddGymForm";
+
 
 function App() {
 
@@ -19,8 +20,15 @@ function App() {
   return (
     <div className='mainpage'>
         <AddGymForm/>
-        <GymCard gyms = {gyms}/>
-      
+        {/* <GymCard /> */}
+      {/* <Router>
+        <Navbar />
+        <Routes>
+          <Route path='' element={<GymCard />} />
+          <Route />
+          <Route  />
+        </Routes>
+      </Router> */}
     </div>
 
   )
