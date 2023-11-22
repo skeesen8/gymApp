f6a8439a145d32cb8d81b1297daa31d4af4535ad
Tom Golebiewski <tgolebie16@gmail.com>
2023-11-22 02:46:12 -0500

    change

--- client/src/components/Gym.js
+++ client/src/components/Gym.js
@@ -11,7 +11,7 @@ import React, {useState} from 'react';
 //             <img onClick ={handleToggle} src={url} alt={name} className='card_image'/>
 //             <div className='card_content'></div>
 //             <div className='card_title'>{name}</div>
-//             <p className='card_text'>{renderDescription ? description : ''}</p> 
+//             {/* <p className='card_text'>{renderDescription ? description : ''}</p>  */}
 //             <div className='card_detail'>
 //                 <p>Name : {name}</p>
 //                 <p>Location : {location}</p>
