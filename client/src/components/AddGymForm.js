68ced2e8fabff6fe847ee8ecd3a976ce58df1831
Sackett <skeesen8@gmail.com>
2023-11-27 14:47:51 -0600

    created add gym form

---
+++ client/src/components/AddGymForm.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { useState } from "react";
+import {useOutletContext} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
+
+function AddGymForm (){
+
+const [newName,setName] = useState('')
+const [newRating,setRating] = useState('')
+const [newUrl,setUrl] = useState('')
+const [newLocation,setLocation]= useState('')
+const [newDescription,setDescription]= useState('')
+
+    function handleName (e){
+        setName(e.target.value)
+    }
+    function handleRating(e){
+        setRating(e.target.value)
+    }
+    function handleUrl(e){
+        setUrl(e.target.value)
+    }
+    function handleLocation(e){
+        setLocation(e.target.value)
+    }
+    function handleDescription(e){
+        setDescription(e.target.value)
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        const newGym = {
+            name:newName,
+            location: newLocation,
+            rating: newRating,
+            url: newUrl,
+            description: newDescription
+        }
+        
+        fetch("http://localhost:5555/",{
+            method:"POST",
+            headers:{
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify(newGym),
+        })
+        .then(response => response.json())
+        // .then(newLegendFromServer => onNewLegend(newLegendFromServer));
+        // navigate("/players")
+
+    }
+
+return(
+    <form>
+        <input
+        value={newName}
+        onChange={handleName}
+        placeholder="Enter Name Here"
+        type='text'
+        name='newName'
+        />
+
+        <input
+        value={newRating}
+        onChange={handleRating}
+        placeholder="add Rating"
+        type = 'text'
+        name = 'newRating'
+        />
+
+        <input
+        value = {newUrl}
+        onChange = {handleUrl}
+        placeholder="add URL Here"
+        type = 'text'
+        name = 'newUrl'
+        />
+
+        <input
+        value = {newDescription}
+        onChange = {handleDescription}
+        placeholder="Add Description"
+        type = 'text'
+        name = 'newDescription'
+        />
+
+        <input
+        value = {newLocation}
+        onChange = {handleLocation}
+        placeholder="Add Location"
+        type = 'text'
+        name = 'newLocation'
+        />
+
+        <button type = 'submit' onClick={handleSubmit} > Add Gym
+        </button>
+       
+    </form>
+
+)
+}
+
+export default AddGymForm
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import AddGymForm from "./AddGymForm";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return( 
+    <div>
+      <h1>Project Client</h1>
+      <AddGymForm/>
+
+
+
+    </div>
+  
+  );
 }
 
 export default App;
