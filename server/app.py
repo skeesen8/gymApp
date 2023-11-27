7f965ff73dd80b8e0ff80724834c07eee79f0000
Tashi Gyatso <Tashigyatso45@gmail.com>
2023-11-27 15:38:07 -0500

    added some restful routing

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
@@ -21,5 +21,29 @@ def index():
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
 
+class Gyms(Resource):
+    def get(self):
+        gyms = [for gym in Gym.query.all()]
+    def post(self):
+        params = request.json
+        try: 
+            new_gym = Gym(
+            name = params['name'],
+            description=params['description'],
+            rating= params['rating'],
+            image= params['url']
+            )
+        except:
+            return make_response({'error':' invalid gym'}, 404)
+        db.session.add(new_gym)
+        db.session.commit()
+        return make_response(new_gym.todict(), 201)
+             
+        
+api.add_resource(Gyms, '/addgym')
 
-'TESTING 123'
+    
+
+
+        
+
