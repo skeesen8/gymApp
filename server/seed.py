4b5c2f7b87f94bb1878ca70eabbd848205a4b7f5
Tashi Gyatso <Tashigyatso45@gmail.com>
2023-11-28 11:04:43 -0500

    seed filed created

--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,77 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, Gym, Review, User
+
+
+
+
+fake = Faker()
+    
+
+
+
+def create_gyms():
+    gyms = []
+    for _ in range(10):
+        a = Gym(
+            name=fake.name(),
+            description=fake.sentence(),
+            rating = randint(1,5),
+            image = fake.sentence()
+        )
+        gyms.append(a)
+
+    return gyms
+
+def create_reviews(users, gyms):
+    reviews = []
+    for _ in range(5):
+        r = Review(
+            name=fake.name(),
+            rating= randint(1,5),
+            review_description = fake.sentence(),
+            gym_id = rc([gym.id for gym in gyms]),
+            user_id = rc([user.id for user in users])
+
+        )
+        reviews.append(r)
+
+    return reviews
+
+def create_users():
+    users = []
+    for _ in range(20):
+        u = User(
+            name = fake.name(),
+            age = rc (range(5,20))
+
+        )
+        users.append(u)
+
+    return users
+
 
 if __name__ == '__main__':
-    fake = Faker()
     with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+        print("Clearing db...")
+        Gym.query.delete()
+        User.query.delete()
+        Review.query.delete()
+
+        print("Seeding gyms...")
+        gyms = create_gyms()
+        db.session.add_all(gyms)
+        db.session.commit()
+
+        print("Seeding users...")
+        users = create_users()
+        db.session.add_all(users)
+        db.session.commit()
+
+        print("Seeding reviews...")
+        reviews = create_reviews(gyms, users)
+        db.session.add_all(reviews)
+        db.session.commit()
+
+        print("Done seeding!")
