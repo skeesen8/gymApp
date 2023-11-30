34f1ba93dd9e06412d6694492b8b487d0f646ffd
Tom Golebiewski <tgolebie16@gmail.com>
2023-11-30 05:51:13 -0500

    updated the home navbar

---
+++ client/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+function Home(){
+    return(
+    <div className='home'>
+        <h1>
+            WELCOME TO OUR GYM REVIEW WEBSITE! 
+        </h1>
+        <h3>please leave a reivew for any of the gyms you have been to or add a new</h3>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFDO46SCt0-m4jMXNdaS6q2OvbPDp1qQFwYA&usqp=CAU'/>
+
+    </div>   
+
+    
+    )
+}
+export default Home
