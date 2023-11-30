8242609b6bae4294d30873d1aba6eafc06be89a3
Tom Golebiewski <tgolebie16@gmail.com>
2023-11-29 20:49:22 -0500

    updated some css and made a delete button

--- client/src/components/GymCard.js
+++ client/src/components/GymCard.js
@@ -9,21 +9,43 @@ function GymCard(){
     .then((resp)=> resp.json())
     .then((gymData)=>setGym(gymData))
   },[])
-    const renderGym = gyms.map((gymObj) =>{
 
-        return(
-            <Gym
-                key={gymObj.id}
-                id = {gymObj.id}
-                name={gymObj.name}
-                rating={gymObj.rating}
-                location={gymObj.location}
-                description={gymObj.description}
-                image={gymObj.image}
-            />
-        )
+  const handleDeleteGym = (gymId) => {
+    // Implement the logic to delete the gym on the server
+    fetch(`http://localhost:5555/gyms/${gymId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
-    return <ul className='cards'>{renderGym}</ul>
+      .then((response) => {
+        if (response.ok) {
+          setGym((prevGyms) => prevGyms.filter((gym) => gym.id !== gymId));
+        } else {
+          console.error('Failed to delete gym');
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  const renderGym = gyms.map((gymObj) => {
+    return (
+      <Gym
+        key={gymObj.id}
+        id={gymObj.id}
+        name={gymObj.name}
+        rating={gymObj.rating}
+        location={gymObj.location}
+        description={gymObj.description}
+        image={gymObj.image}
+        onDelete={handleDeleteGym}
+      />
+    );
+  });
+
+  return <ul className='cards'>{renderGym}</ul>;
 }
 
 export default GymCard
